47b83c4e2373453da8c6c94ff34e3875e2e9af54
Patricio Testolin <testolinp@gmail.com>
2015-10-13 22:48:42 -0300

    moment js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     app.import('vendor/music-player/music-player.css');
-    app.import('bower_components/moment/moment.js');
-    app.import('bower_components/moment-duration-format/lib/moment-duration-format.js');
+    app.import(app.bowerDirectory + '/moment/moment.js');
+    app.import(app.bowerDirectory + '/moment-duration-format/lib/moment-duration-format.js');
   }
 };
