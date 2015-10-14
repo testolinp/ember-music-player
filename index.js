0c680fafb6970596973b00794117809534cd716c
Patricio Testolin <testolinp@gmail.com>
2015-10-13 23:11:58 -0300

    bower moment duration

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
 
 module.exports = {
   name: 'ember-music-player',
-  
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
   included: function(app) {
     this._super.included(app);
     app.import('vendor/music-player/music-player.css');
